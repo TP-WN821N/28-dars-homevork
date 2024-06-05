4979d2a2e532df3e582dedb8a9a73dfa3ba99b73
TP-WN821N <azikmelor7705@gmail.com>
2024-06-05 17:33:51 +0500

    js header_shrink

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const headerList = document.querySelector('.header__list')
 const menu = document.getElementById('menu')
+const header = document.querySelector('.header')
 
 menu.addEventListener('click', () => {
   headerList.classList.toggle('toggle_menu')
+})
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 10) {
+    header.classList.add('header_shrink')
+  }
+  else {
+    header.classList.remove('header_shrink')
+  }
 })
